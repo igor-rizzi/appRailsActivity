63b322e4170091fed728c513044d71a4a620ec3e
Igor Henrique Rizzi <igor.rizzi2@gmail.com>
2022-09-27 22:27:01 -0300

    commit de controller modificada e tabela dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class DashboardController < ApplicationController
   def index
+    @activities = Activity.all
+    @groups = Group.all
+
+    @groups.each do |group|
+      @grups_activities = Activity.where(group_id: group.id).length
+    end
   end
 end
